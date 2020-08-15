9da370d129ab2138c81213da7ee269ab6418577b
vasu potla <potlajobs@gmail.com>
2020-08-15 15:51:00 -0400

    upgrading to latest angular

--- SampleAuth/src/app/app.module.ts
+++ SampleAuth/src/app/app.module.ts
@@ -14,8 +14,7 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
